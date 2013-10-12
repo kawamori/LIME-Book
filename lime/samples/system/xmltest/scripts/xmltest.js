99dffd9cd4595d3840df59f6e4cec4a99a65e994
Masahito Kawamori <kawamorim@gmail.com>
2013-10-12 22:11:50 +0900

    modified on 12 Oct 2013

--- lime/samples/system/xmltest/scripts/xmltest.js
+++ lime/samples/system/xmltest/scripts/xmltest.js
@@ -42,8 +42,12 @@ alert("called from xmltest.js\n");
 
 alert("xml? "+splitMulti(array[2]));
 
+var partXML = splitMulti(array[2]);
 	// 改行コード除去
-	var str = deleteLineFeed(array[2]);
+/*	var str = deleteLineFeed(array[2]);*/
+var str = deleteLineFeed(partXML);
+
+alert("str: "+str);
 
 	var captionCounter = 0;
 	var startEndFlag = 0;
