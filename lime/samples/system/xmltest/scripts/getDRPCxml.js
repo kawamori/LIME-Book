5102e2018549e10752ec1849c35b3b82ff6786dd
Masahito Kawamori <kawamorim@gmail.com>
2013-10-13 19:56:27 +0900

    modified on 13 Oct 2013

--- lime/samples/system/xmltest/scripts/getDRPCxml.js
+++ lime/samples/system/xmltest/scripts/getDRPCxml.js
@@ -77,7 +77,7 @@ alert("calling getDRPCxml.js\n");
 					
 					alert(" tag is "+rootObj.contents[0].contents[0].contents[5].contents[5].name);
                               for (var pname in rootObj.contents[0].contents[0].contents[5].contents[5].attributes){
-                              	       alert("pname\t" +rootObj.contents[0].contents[0].contents[5].contents[5].attributes[pname]);
+                              	       alert("pname\t"+pname +rootObj.contents[0].contents[0].contents[5].contents[5].attributes[pname]);
                          			  }
                          			  
 				
