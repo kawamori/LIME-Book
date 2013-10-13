5082d7e4327855481379fea1958bd7c9423883c4
Masahito Kawamori <kawamorim@gmail.com>
2013-10-13 18:31:39 +0900

    modified on 13 Oct 2013

--- lime/samples/system/xmltest/scripts/xmltest.js
+++ lime/samples/system/xmltest/scripts/xmltest.js
@@ -63,10 +63,12 @@ alert("called from xmltest.js\n");
 					}*/
 					
 				    	                for(var i=0; i<rootObj.contents[0].contents[0].contents[5].contents.length; i++){
+				    	                	if (rootObj.contents[0].contents[0].contents[5].contents[i].name){
 alert("PERMISSION_CLASSIFICATION_UNIT "+i+"th tag is "+rootObj.contents[0].contents[0].contents[5].contents[i].name); 
                                      for (var pname in rootObj.contents[0].contents[0].contents[5].contents[i].attributes){
                                      alert("value is "+rootObj.contents[0].contents[0].contents[0].contents[i].attributes[pname]);
                                          }
+                                            }
 		              			}
 					
 					
