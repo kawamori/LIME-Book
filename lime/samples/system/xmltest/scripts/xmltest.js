712a93816a1f60b61ff3bf5e57da480be524a695
Masahito Kawamori <kawamorim@gmail.com>
2013-10-13 19:09:26 +0900

    modified on 13 Oct 2013

--- lime/samples/system/xmltest/scripts/xmltest.js
+++ lime/samples/system/xmltest/scripts/xmltest.js
@@ -64,10 +64,12 @@ alert("called from xmltest.js\n");
 					
 				    	                for(var i=0; i<rootObj.contents[0].contents[0].contents[5].contents.length; i++){ 
 				    	                	if (rootObj.contents[0].contents[0].contents[5].contents[i].name){
-                                            
-                   alert("PERMISSION_CLASSIFICATION_UNIT "+ i+ "th tag is "+rootObj.contents[0].contents[0].contents[5].contents[i].name); 
-                                              }
-		              			}
+                              for (var pname in rootObj.contents[0].contents[0].contents[5].contents[i].attributes){
+                              	       alert("pname "+rootObj.contents[0].contents[0].contents[5].contents[i].attributes[pname]);
+                   /*alert("PERMISSION_CLASSIFICATION_UNIT "+ i+ "th tag is "+rootObj.contents[0].contents[0].contents[5].contents[i].name); */
+                   			  }
+                           }
+		              	}
 					
 					
 					alert("next next? "+rootObj.contents[0].contents[0].contents[2].name);/* gives the tag name */
